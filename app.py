c2f95db43f662c3f7562c58f6e9cc16d966f1809
steno86 <98324464+steno86@users.noreply.github.com>
2024-08-08 21:07:33 +0200

    +1

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 import psutil
@@ -7,16 +7,18 @@ import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///devices.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///instance/devices.db'
 db = SQLAlchemy(app)
 login_manager = LoginManager()
 login_manager.init_app(app)
+login_manager.login_view = 'login'
 
 # Datenbankmodelle
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password = db.Column(db.String(80), nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
     can_add_devices = db.Column(db.Boolean, default=False)
     can_send_wol = db.Column(db.Boolean, default=False)
 
@@ -40,6 +42,11 @@ def get_ip_address(interface):
 def init_db():
     with app.app_context():
         db.create_all()
+        # Erstelle einen Admin-Benutzer, wenn keiner existiert
+        if not User.query.filter_by(username='admin').first():
+            admin = User(username='admin', password='adminpass', is_admin=True, can_add_devices=True, can_send_wol=True)
+            db.session.add(admin)
+            db.session.commit()
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -50,6 +57,8 @@ def login():
         if user and user.password == password:
             login_user(user)
             return redirect(url_for('index'))
+        else:
+            flash('Login fehlgeschlagen. Bitte überprüfen Sie Ihre Anmeldedaten.')
     return render_template('login.html')
 
 @app.route('/logout')
@@ -86,6 +95,28 @@ def wake_device(device_id):
             ip_address = get_ip_address(device.interface)
             send_magic_packet(device.mac, ip_address=ip_address)
     return redirect(url_for('index'))
+
+@app.route('/admin')
+@login_required
+def admin():
+    if not current_user.is_admin:
+        return redirect(url_for('index'))
+    users = User.query.all()
+    return render_template('admin.html', users=users)
+
+@app.route('/admin/add_user', methods=['POST'])
+@login_required
+def add_user():
+    if not current_user.is_admin:
+        return redirect(url_for('index'))
+    username = request.form['username']
+    password = request.form['password']
+    can_add_devices = 'can_add_devices' in request.form
+    can_send_wol = 'can_send_wol' in request.form
+    new_user = User(username=username, password=password, can_add_devices=can_add_devices, can_send_wol=can_send_wol)
+    db.session.add(new_user)
+    db.session.commit()
+    return redirect(url_for('admin'))
 
 if __name__ == '__main__':
     init_db()
